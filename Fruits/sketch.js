08268ba49646fbaded56a46abc84c4370868e8d6
unknown <raveen.anand@whitehatjr.com>
2021-08-12 13:16:22 +0530

    First

---
+++ Fruits/sketch.js
@@ -0,0 +1,13 @@
+var fruits = ["Apple", "Mangoe", "Banana", "orange", "Strawberry", "watermelon", "grapes"];
+
+function setup() {
+
+  for (i=0; i<fruits.length; i++){
+    console.log(fruits[i]);
+  }
+}
+
+
+function draw() {
+    
+}
