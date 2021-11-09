46021104bbed2c184d340dcf82c175f1ae4e5217
unknown <raveen.anand@whitehatjr.com>
2021-11-09 13:17:03 +0530

    v2

--- Fruits/sketch.js
+++ Fruits/sketch.js
@@ -2,7 +2,13 @@ var fruits = ["Apple", "Mango", "Banana", "Orange"];
 
 function setup() {
 
-  
+  for (i=0; i<fruits.length; i++){
+    console.log(fruits[i]);
+  }
+  /**SAA2:**/
+  /*for (i=0; i<3; i++){
+    console.log(fruits[i]);
+  }*/
 }
 
 function draw() {
