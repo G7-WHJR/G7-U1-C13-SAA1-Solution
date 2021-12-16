bedde632154e9d324cba46e463fb3975a610e098
unknown <raveen.anand@whitehatjr.com>
2021-12-16 17:36:42 +0530

    v2

--- Fruits/sketch.js
+++ Fruits/sketch.js
@@ -1,14 +1,16 @@
+//SAA1 :
 var fruits = ["Apple", "Mango", "Banana", "Orange"];
 
 function setup() {
 
+  //SAA1 :
   for (i=0; i<fruits.length; i++){
     console.log(fruits[i]);
   }
-  /**SAA2:**/
-  /*for (i=0; i<3; i++){
+  //SAA2:
+  for (i=0; i<3; i++){
     console.log(fruits[i]);
-  }*/
+  }
 }
 
 function draw() {
